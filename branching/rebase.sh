90094c03706e83c1c399a437a45ba709655ce777
Maks Doshuk <doshuk_fox10@mail.ru>
2022-03-26 10:09:40 -0400

    git rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -7,6 +7,10 @@
 count=1
 for param in "$@"; do
     echo "\$@ Parameter #$count = $param"
+=======
+for param in "$*"; do
+    echo "\$* Next parameter #$count = $param"
+>>>>>>> bad665d (git rebase 2)
     count=$(( $count + 1 ))
 done
 
